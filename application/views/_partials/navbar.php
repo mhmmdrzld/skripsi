f7e643f801e3759494507ff8b372c1f604e06004
muhammad rizaldi <mrizaldi1998@gmail.com>
2021-08-21 21:50:33 +0800

    menu operator

--- application/views/_partials/navbar.php
+++ application/views/_partials/navbar.php
@@ -16,9 +16,35 @@
                     <a href="<?= site_url($this->uri->segment(1) . '/Beranda'); ?>" class="nav-link">Beranda</a>
                 </li>
 
+                <!-- Menu Admin -->
                 <?php if ($_SESSION['level'] === 'Admin') { ?>
                     <li class="nav-item">
                         <a href="<?= site_url('admin/Sekolah'); ?>" class="nav-link">Data Sekolah</a>
+                    </li>
+                <?php } ?>
+
+                <!-- Menu Operator -->
+                <?php if ($_SESSION['level'] === 'Operator') { ?>
+                    <li class="nav-item dropdown">
+                        <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Data Master</a>
+                        <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                            <li><a href="<?= site_url('operator/Sekolah'); ?>" class="dropdown-item">Data Sekolah</a></li>
+                            <li><a href="<?= site_url('operator/Siswa'); ?>" class="dropdown-item">Data Siswa</a></li>
+                            <li><a href="<?= site_url('operator/Kelas'); ?>" class="dropdown-item">Data Kelas</a></li>
+                            <li><a href="<?= site_url('operator/Jurusan'); ?>" class="dropdown-item">Data Jurusan</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Data Kegiatan</a>
+                        <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+                            <li><a href="<?= site_url('operator/Eskul'); ?>" class="dropdown-item">Data Esktrakurikuler</a></li>
+                            <li><a href="<?= site_url('operator/Anggota'); ?>" class="dropdown-item">Data Anggota</a></li>
+                            <li><a href="<?= site_url('operator/Jadwal'); ?>" class="dropdown-item">Data Jadwal</a></li>
+                            <li><a href="<?= site_url('operator/Jabatan'); ?>" class="dropdown-item">Data Jabatan</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= site_url('admin/Berita'); ?>" class="nav-link">Data Berita</a>
                     </li>
                 <?php } ?>
 
